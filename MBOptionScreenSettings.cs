59bda16f7a7bcdddbe7ad01cd3b7ee963a34db4e
Vitaly Mikhailov <personal@aragas.org>
2020-04-16 03:28:04 +0300

    XmlElement can be injected later if necessary, should not be required

--- MBOptionScreenSettings.cs
+++ MBOptionScreenSettings.cs
@@ -1,15 +1,11 @@
 ﻿using MBOptionScreen.Settings;
 
-using System.Xml.Serialization;
-
 namespace MBOptionScreen
 {
     internal class MBOptionScreenSettings : AttributeSettings<MBOptionScreenSettings>
     {
+        public override string Id { get; set; } = "OptionScreen_v1";
         public override string ModName => $"OptionScreen v1";
         public override string ModuleFolderName => "OptionScreen";
-
-        [XmlElement]
-        public override string Id { get; set; } = "OptionScreen_v1";
     }
 }
--- TestSettings.cs
+++ TestSettings.cs
@@ -1,47 +1,39 @@
 ﻿using MBOptionScreen.Attributes;
 using MBOptionScreen.Settings;
 
-using System.Xml.Serialization;
-
 namespace MBOptionScreen
 {
     internal class TestSettings : AttributeSettings<TestSettings>
     {
-        public override string ModName => "Testing";
-        public override string ModuleFolderName => "Testing";
         public const string SettingsInstanceID = "Testing";
 
-        [XmlElement]
         public override string Id { get; set; } = SettingsInstanceID;
-        [XmlElement]
+        public override string ModName => "Testing";
+        public override string ModuleFolderName => "Testing";
+
+
         [SettingProperty("Enable Crash Error Reporting", false, "When enabled, shows a message box showing the cause of a crash.")]
         [SettingPropertyGroup("Debugging")]
         public bool DebugMode { get; set; } = true;
 
-        [XmlElement]
         [SettingProperty("Test Property 1", false, "")]
         [SettingPropertyGroup("Debugging/Test Group", true)]
         public bool TestProperty1 { get; set; } = false;
 
-        [XmlElement]
         [SettingProperty("Test Property 5", false, "")]
         [SettingPropertyGroup("Debugging/Test Group")]
         public bool TestProperty5 { get; set; } = false;
 
-        [XmlElement]
         [SettingProperty("Test Property 2", false, "")]
         [SettingPropertyGroup("Debugging/Test Group/Test Group 2", true)]
         public bool TestProperty2 { get; set; } = false;
 
-        [XmlElement]
         [SettingProperty("Test Property 4", 0f, 0.5f, 0f, 100f, false, "")]
         [SettingPropertyGroup("Debugging/Test Group/Test Group 2")]
         public float TestProperty4 { get; set; } = 0.2f;
 
-        [XmlElement]
         [SettingProperty("Test Property 3", 0, 10, true, "")]
         [SettingPropertyGroup("Debugging/Test Group/Test Group 3")]
         public int TestProperty3 { get; set; } = 2;
-
     }
 }
