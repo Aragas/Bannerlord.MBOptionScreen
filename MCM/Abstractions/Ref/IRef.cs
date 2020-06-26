196dee44e7f0654b7ef143e1bd5bae39512efadb
Vitaly Mikhailov <personal@aragas.org>
2020-06-26 17:56:40 +0300

    Update IRef.cs

--- MCM/Abstractions/Ref/IRef.cs
+++ MCM/Abstractions/Ref/IRef.cs
@@ -3,6 +3,9 @@ using System.ComponentModel;
 
 namespace MCM.Abstractions.Ref
 {
+    /// <summary>
+    /// An interface that provides access to some value for MCM to get/set.
+    /// </summary>
     public interface IRef : INotifyPropertyChanged
     {
         Type Type { get; }
