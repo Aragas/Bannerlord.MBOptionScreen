38f60eb36c1a5a1fed4d75e77b31052d8707824c
Vitaly Mikhailov <personal@aragas.org>
2021-01-17 03:28:40 +0300

    Restored DefaultDropdown

--- src/MCM/Abstractions/Data/DefaultDropdown.cs
+++ src/MCM/Abstractions/Data/DefaultDropdown.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Bannerlord.BUTR.Shared.Helpers;
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using TaleWorlds.Core.ViewModelCollection;
 
@@ -9,12 +12,59 @@ namespace MCM.Abstractions.Data
     {
         public static DefaultDropdown<T> Empty => new(Array.Empty<T>(), 0);
 
-        public SelectorVM<SelectorItemVM> Selector { get => null!; set { } }
+        private SelectorVM<SelectorItemVM> _selector;
+        private int _selectedIndex;
+        public SelectorVM<SelectorItemVM> Selector
+        {
+            get => _selector;
+            set
+            {
+                if (_selector != value)
+                {
+                    _selector = value;
+                    _selector.SetOnChangeAction(OnSelectionChanged);
+                }
+            }
+        }
 
-        public int SelectedIndex { get => -1; set { } }
-        public T SelectedValue { get => default!; set { } }
+        private void OnSelectionChanged(SelectorVM<SelectorItemVM> obj)
+        {
+            _selectedIndex = obj.SelectedIndex;
+        }
 
-        public DefaultDropdown(IEnumerable<T> values, int selectedIndex) : base(values) { }
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                if (_selectedIndex != value)
+                {
+                    _selectedIndex = value;
+                    Selector.SelectedIndex = _selectedIndex;
+                }
+            }
+        }
+        public T SelectedValue
+        {
+            get => this[SelectedIndex];
+            set
+            {
+                var index = IndexOf(value);
+                if (index == -1)
+                    return;
+                SelectedIndex = index;
+            }
+        }
+
+        public DefaultDropdown(IEnumerable<T> values, int selectedIndex) : base(values)
+        {
+            _selector = new SelectorVM<SelectorItemVM>(this.Select(x => TextObjectHelper.Create(x?.ToString() ?? "ERROR").ToString()),
+                selectedIndex,
+                OnSelectionChanged);
+
+            if (SelectedIndex != 0 && SelectedIndex >= Count)
+                throw new Exception();
+        }
 
         public bool Equals(DefaultDropdown<T> x, DefaultDropdown<T> y) => x.SelectedIndex == y.SelectedIndex;
         public int GetHashCode(DefaultDropdown<T> obj) => obj.SelectedIndex;
