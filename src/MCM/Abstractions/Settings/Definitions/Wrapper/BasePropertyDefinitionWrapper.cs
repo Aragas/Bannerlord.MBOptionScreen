5528dca4efb8f884a10d1c79b68be87e6f1842b8
Vitaly Mikhailov <personal@aragas.org>
2021-08-16 16:49:14 +0300

    Fix #113

--- src/MCM/Abstractions/Settings/Definitions/Wrapper/BasePropertyDefinitionWrapper.cs
+++ src/MCM/Abstractions/Settings/Definitions/Wrapper/BasePropertyDefinitionWrapper.cs
@@ -17,7 +17,7 @@ namespace MCM.Abstractions.Settings.Definitions.Wrapper
         {
             var type = @object.GetType();
 
-            DisplayName = TextObjectHelper.Create(type.GetProperty(nameof(DisplayName))?.GetValue(@object) as string ?? "ERROR")?.ToString() ?? "ERROR";;
+            DisplayName = TextObjectHelper.Create(type.GetProperty(nameof(DisplayName))?.GetValue(@object) as string ?? "ERROR")?.ToString() ?? "ERROR";
             Order = type.GetProperty(nameof(Order))?.GetValue(@object) as int? ?? -1;
             RequireRestart = type.GetProperty(nameof(RequireRestart))?.GetValue(@object) as bool? ?? true;
             HintText = TextObjectHelper.Create(type.GetProperty(nameof(HintText))?.GetValue(@object) as string ?? "ERROR")?.ToString() ?? "ERROR";
