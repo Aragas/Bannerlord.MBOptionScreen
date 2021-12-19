b200eace4564c6d52445937c0cf0d0041c947fc9
Aragas <personal@aragas.org>
2021-12-19 20:39:20 +0300

    Fixed resource loading
    (cherry picked from commit 4ae1c62325c1f8cf57c050169008f23391f776ab)

--- src/MCM.UI/MCMUISubModule.cs
+++ src/MCM.UI/MCMUISubModule.cs
@@ -119,6 +119,9 @@ namespace MCM.UI
             var viewmodelwrapperHarmony = new Harmony("bannerlord.mcm.ui.viewmodelpatch");
             ViewModelPatch.Patch(viewmodelwrapperHarmony);
 
+            var optionsGauntletScreenHarmony = new Harmony("bannerlord.mcm.ui.optionsgauntletscreenpatch");
+            OptionsGauntletScreenPatch.Patch(optionsGauntletScreenHarmony);
+
 
             DelayedSubModuleManager.Register<SandBoxSubModule>();
             DelayedSubModuleManager.Subscribe<SandBoxSubModule, MCMUISubModule>(
---
+++ src/MCM.UI/Patches/OptionsGauntletScreenPatch.cs
@@ -0,0 +1,58 @@
+﻿using Bannerlord.BUTR.Shared.Helpers;
+
+using HarmonyLib;
+using HarmonyLib.BUTR.Extensions;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.MountAndBlade.GauntletUI;
+using TaleWorlds.TwoDimension;
+
+namespace MCM.UI.Patches
+{
+    internal static class OptionsGauntletScreenPatch
+    {
+        private static readonly ConditionalWeakTable<OptionsGauntletScreen, SpriteCategory> _spriteCategorySaveLoads = new();
+
+        public static void Patch(Harmony harmony)
+        {
+            harmony.Patch(
+                AccessTools2.Method(typeof(OptionsGauntletScreen), "OnInitialize"),
+                postfix: new HarmonyMethod(typeof(OptionsGauntletScreenPatch), nameof(OnInitializePostfix)));
+
+            harmony.Patch(
+                AccessTools2.Method(typeof(OptionsGauntletScreen), "OnFinalize"),
+                postfix: new HarmonyMethod(typeof(OptionsGauntletScreenPatch), nameof(OnFinalizePostfix)));
+        }
+
+        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "For ReSharper")]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void OnInitializePostfix(OptionsGauntletScreen __instance)
+        {
+            if (ApplicationVersionHelper.GameVersion() is { } gameVersion)
+            {
+                if ((gameVersion.Major >= 1 && gameVersion.Minor >= 6 && gameVersion.Revision >= 1) || (gameVersion.Major >= 1 && gameVersion.Minor >= 7))
+                {
+                    var spriteCategorySaveLoad = UIResourceManager.SpriteData.SpriteCategories.TryGetValue("ui_saveload", out var spriteCategorySaveLoadVal)
+                            ? spriteCategorySaveLoadVal
+                            : null;
+                    spriteCategorySaveLoad?.Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
+                    _spriteCategorySaveLoads.Add(__instance, spriteCategorySaveLoad);
+                }
+            }
+        }
+
+        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "For ReSharper")]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void OnFinalizePostfix(OptionsGauntletScreen __instance)
+        {
+
+        }
+    }
+}
