210af7c947a5dcaa1d414c3707864906cbcc84ff
Aragas <personal@aragas.org>
2022-01-28 15:26:38 +0300

    Fixed e1.7.1 compatibility

--- src/MCM.UI/Utils/EscapeMenuItemVMUtils.cs
+++ src/MCM.UI/Utils/EscapeMenuItemVMUtils.cs
@@ -13,9 +13,11 @@ namespace MCM.UI.Utils
     {
         private delegate EscapeMenuItemVM V1Delegate(TextObject item, Action<object> onExecute, object identifier, bool isDisabled, bool isPositiveBehavioured = false);
         private delegate EscapeMenuItemVM V2Delegate(TextObject item, Action<object> onExecute, object identifier, Tuple<bool, TextObject> isDisabledAndReason, bool isPositiveBehavioured = false);
+        private delegate EscapeMenuItemVM V3Delegate(TextObject item, Action<object> onExecute, object identifier, Func<Tuple<bool, TextObject>> isDisabledAndReason, bool isPositiveBehavioured = false);
 
         private static readonly V1Delegate? V1;
         private static readonly V2Delegate? V2;
+        private static readonly V3Delegate? V3;
 
         static EscapeMenuItemVMUtils()
         {
@@ -30,6 +32,9 @@ namespace MCM.UI.Utils
                     case 5 when @params[4].ParameterType == typeof(Tuple<bool, TextObject>):
                         V2 = AccessTools2.GetDelegate<V2Delegate>(constructorInfo);
                         break;
+                    case 5 when @params[4].ParameterType == typeof(Func<Tuple<bool, TextObject>>):
+                        V3 = AccessTools2.GetDelegate<V3Delegate>(constructorInfo);
+                        break;
                 }
             }
         }
@@ -40,6 +45,8 @@ namespace MCM.UI.Utils
                 return V1(item, onExecute, identifier, isDisabled, isPositiveBehavioured);
             if (V2 is not null)
                 return V2(item, onExecute, identifier, new Tuple<bool, TextObject>(isDisabled, TextObjectHelper.Create(string.Empty)!), isPositiveBehavioured);
+            if (V3 is not null)
+                return V3(item, onExecute, identifier, () => new Tuple<bool, TextObject>(isDisabled, TextObjectHelper.Create(string.Empty)!), isPositiveBehavioured);
             return null;
         }
     }
