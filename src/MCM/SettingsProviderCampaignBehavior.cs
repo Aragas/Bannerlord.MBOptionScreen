5b184f1d0359319d9a26faadb7dbd7ef6b26c8e4
aragas <va_mikhaylov@head-point.ru>
2022-03-28 12:39:49 +0300

    Added a Campaign and Mission Behaviors for accessing the settings in an OOP way
    Better PackageReference formatting

---
+++ src/MCM/SettingsProviderCampaignBehavior.cs
@@ -0,0 +1,31 @@
+﻿using MCM.Abstractions.Settings.Base;
+using MCM.Abstractions.Settings.Providers;
+
+using System;
+using System.Collections.Concurrent;
+
+using TaleWorlds.CampaignSystem;
+
+namespace MCM
+{
+    public class SettingsProviderCampaignBehavior : CampaignBehaviorBase
+    {
+        private readonly BaseSettingsProvider? _baseSettingsProvider;
+        private readonly ConcurrentDictionary<Type, string> _cache = new();
+
+        public SettingsProviderCampaignBehavior(BaseSettingsProvider? baseSettingsProvider)
+        {
+            _baseSettingsProvider = baseSettingsProvider;
+        }
+
+        public TSettings? Get<TSettings>() where TSettings : BaseSettings, new()
+        {
+            if (!_cache.ContainsKey(typeof(TSettings)))
+                _cache.TryAdd(typeof(TSettings), new TSettings().Id);
+            return _baseSettingsProvider?.GetSettings(_cache[typeof(TSettings)]) as TSettings;
+        }
+
+        public override void RegisterEvents() { }
+        public override void SyncData(IDataStore dataStore) { }
+    }
+}
