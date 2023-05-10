6f096afbb2126601afccfe14c2fc5a38ef9c144f
Vitalii Mikhailov <personal@aragas.org>
2023-05-10 17:40:56 +0300

    Removed unused widget, added autogen support back

--- src/MCM.UI/UIExtenderEx/OptionsPrefabExtensions.cs
+++ src/MCM.UI/UIExtenderEx/OptionsPrefabExtensions.cs
@@ -6,7 +6,7 @@ using System.Xml;
 
 namespace MCM.UI.UIExtenderEx
 {
-    [PrefabExtension("Options", "descendant::ListPanel[@Id='TabToggleList']/Children/OptionsTabToggle[5]")]
+    [PrefabExtension("Options", "descendant::ListPanel[@Id='TabToggleList']/Children/OptionsTabToggle[5]", "Options")]
     internal sealed class OptionsPrefabExtension1 : PrefabExtensionInsertPatch
     {
         public override InsertType Type => InsertType.Prepend;
@@ -22,7 +22,7 @@ namespace MCM.UI.UIExtenderEx
         public XmlNode GetPrefabExtension() => _xmlDocument;
     }
 
-    [PrefabExtension("Options", "descendant::TabControl[@Id='TabControl']/Children/*[5]")]
+    [PrefabExtension("Options", "descendant::TabControl[@Id='TabControl']/Children/*[5]", "Options")]
     internal sealed class OptionsPrefabExtension2 : PrefabExtensionInsertPatch
     {
         public override InsertType Type => InsertType.Prepend;
@@ -38,7 +38,7 @@ namespace MCM.UI.UIExtenderEx
         public XmlDocument GetPrefabExtension() => _xmlDocument;
     }
 
-    [PrefabExtension("Options", "descendant::Widget[@Id='DescriptionsRightPanel']")]
+    [PrefabExtension("Options", "descendant::Widget[@Id='DescriptionsRightPanel']", "Options")]
     internal sealed class OptionsPrefabExtension3 : PrefabExtensionSetAttributePatch
     {
         public override List<Attribute> Attributes => new()
