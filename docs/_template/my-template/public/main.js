d4484ee934517b57ac5793a620d412372a7794ee
Vitalii Mikhailov <personal@aragas.org>
2024-01-07 20:00:33 +0200

    DocFX Update

---
+++ docs/_template/my-template/public/main.js
@@ -0,0 +1,15 @@
+export default {
+  defaultTheme: 'dark',
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/Aragas/Bannerlord.MBOptionScreen',
+      title: 'GitHub'
+    },
+    {
+      icon: 'discord',
+      href: 'https://discord.gg/unBY2twS3V',
+      title: 'Discord'
+    }
+  ]
+}
